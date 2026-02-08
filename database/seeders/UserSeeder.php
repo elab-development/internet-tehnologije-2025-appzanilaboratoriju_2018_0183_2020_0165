d2b3ced144052b7ce25846abf2b3404919390f95
Mihajlo <acimovicmihajlo18@gmail.com>
2026-02-08 20:23:21 +0100

    Onemogucen pristup nekim funkcijama ukoliko korisnik nije autentifikovan

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,26 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Uloga;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        $testUser = User::create([
+            'ImePrezime' => 'Test Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123'),
+            'Biografija' => 'Ovo je test nalog za postman.'
+        ]);
+        $testUser->uloge()->attach(1, [
+            'datum' => Carbon::now()
+        ]); 
+
         User::factory(10)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,4 @@
-<?php   
+<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -7,14 +7,18 @@ use App\Http\Controllers\StavkaRecenzijeController;
 use App\Http\Controllers\NaucniRadController;
 use App\Http\Controllers\AuthController;
 
-Route::apiResource('users', UserController::class);
-Route::apiResource('recenzije', RecenzijaController::class);
-Route::apiResource('stavke-recenzije', StavkaRecenzijeController::class);
-Route::apiResource('naucniRadovi', NaucniRadController::class);
+// JAVNE RUTE (Dostupne svima)
 Route::post('/login', [AuthController::class, 'login']);
+
+// ZAŠTIĆENE RUTE (Zahtev 10 - samo za ulogovane korisnike)
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    
+    // Tvoji postojeći resursi su sada zaštićeni
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('recenzije', RecenzijaController::class);
+    Route::apiResource('stavke-recenzije', StavkaRecenzijeController::class);
+    Route::apiResource('naucniRadovi', NaucniRadController::class);
+
+    // Logout funkcija koju imaju samo ulogovani
     Route::post('/logout', [AuthController::class, 'logout']);
-}); // ovo je funkcija koju imaju samo ulogovani ljudi
-
-
-
+});
